f93757789357065a82e34f828166589f0ce0cb2f
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-04-24 15:31:14 +0300

    Basic Syntax, Conditional Statements and Loops Lab

---
+++ Basic Syntax, Conditional Statements and Loops Lab/excellentGrade.js
@@ -0,0 +1,9 @@
+function solve(grade) {
+        console.log(grade >= 5.50 ? "Excellent" : "Not excellent");
+    
+}
+
+solve(4.50);
+solve(5.00);
+solve(5.50);
+solve(6.00);
---
+++ Basic Syntax, Conditional Statements and Loops Lab/numberFromNto1.js
@@ -0,0 +1,13 @@
+function solve(num) {
+    num++;
+    while(num--) {
+        if (num <= 0) {
+            break;
+        }
+
+        console.log(num);
+    }
+}
+
+solve(5);
+solve(3);
